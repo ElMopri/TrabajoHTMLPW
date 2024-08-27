4e0eff4977356a4040e4c623724f330dfaea2cab
ElMopri <alopeztriana1@gmail.com>
2024-08-26 21:04:24 -0500

    se agrego el codigo js para el registro.html

--- scripts.js
+++ scripts.js
@@ -1,14 +1,31 @@
 document.addEventListener("DOMContentLoaded",function(){
     var loginForm = document.getElementById("loginForm");
-    loginForm.addEventListener("submit",function(event){
-        event.preventDefault();
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
+    if(loginForm){
+        loginForm.addEventListener("submit",function(event){
+            event.preventDefault();
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
 
-        if(username == "admin" && password=="1234"){
-            window.location.href = "dashboard.html";
-        } else {
-            alert("Usuario o contraseña incorrectos");
-        }
-    });
+            //Verificacion de usuario y contraseña
+            if(username == "admin" && password=="1234"){
+                window.location.href = "dashboard.html";
+            } else {
+                alert("Usuario o contraseña incorrectos");
+            }
+        });
+    }
+
+    var registroForm = document.getElementById("registroForm");
+    if(registroForm){
+        registroForm.addEventListener("submit",function(event){
+            event.preventDefault();
+            var newUser = document.getElementById("newUser").value;
+            var newPassword = document.getElementById("newPassword").value;
+
+            //Aqui va la logica para almacenar al usuario registrado
+            
+            alert("Usuario regitrado: " + newUser);
+            window.location.href = "index.html";
+        });
+    }
 });
